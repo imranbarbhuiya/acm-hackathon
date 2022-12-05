180670f00ffc34c34bda34330509384208913b9f
parbez <imranbarbhuiya.fsd@gmail.com>
2022-12-05 10:54:28 +0530

    chore: remove unocss completly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
-import '@unocss/reset/tailwind.css';
 import '../styles/globals.css';
-import '../styles/unocss.css';
 import RootStyleRegistry from './emotion';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
